035d520bd141c438a5770442f3664aa55f35bf23
Miguel Hermo Serans <miguel.hermo@gmail.com>
2022-12-02 15:46:24 +0200

    final version day2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -17,26 +17,28 @@ fn shape_value(c: char) -> i32 {
 
 fn main() {
     let game1_points: HashMap<(i32,i32),i32> =
-        [((0 , 0)  ,4),
-         ((0 , 1)  ,1),
-         ((0 , 2)  ,7),
-         ((1 , 0)  ,8),
-         ((1 , 1)  ,5),
-         ((1 , 2)  ,2),
-         ((2 , 0)  ,3),
-         ((2 , 1)  ,9),
-         ((2 , 2)  ,6),].iter().cloned().collect();
+    // (opponent, me), points_for_my_move + points_vs_opponent
+        [((0, 0), 1 + 3),
+         ((1, 0), 1 + 0),
+         ((2, 0), 1 + 6),
+         ((0, 1), 2 + 6),
+         ((1, 1), 2 + 3),
+         ((2, 1), 2 + 0),
+         ((0, 2), 3 + 0),
+         ((1, 2), 3 + 6),
+         ((2, 2), 3 + 3),].iter().cloned().collect();
 
     let game2_points: HashMap<(i32,i32),i32> =
-        [((0 , 0)  ,3+0),
-         ((0 , 1)  ,1+3),
-         ((0 , 2)  ,2+6),
-         ((1 , 0)  ,1+0),
-         ((1 , 1)  ,2+3),
-         ((1 , 2)  ,3+6),
-         ((2 , 0)  ,2+0),
-         ((2 , 1)  ,3+3),
-         ((2 , 2)  ,1+6),].iter().cloned().collect();
+    // (opponent, desired_result), points_for_my_move + points_vs_opponent
+        [((0, 0), 3 + 0),
+         ((0, 1), 1 + 3),
+         ((0, 2), 2 + 6),
+         ((1, 0), 1 + 0),
+         ((1, 1), 2 + 3),
+         ((1, 2), 3 + 6),
+         ((2, 0), 2 + 0),
+         ((2, 1), 3 + 3),
+         ((2, 2), 1 + 6),].iter().cloned().collect();
     
     let mut score1 = 0;
     let mut score2 = 0;
@@ -49,8 +51,8 @@ fn main() {
         let a = shape_value(line.chars().nth(0).unwrap());
         let b = shape_value(line.chars().nth(2).unwrap());
 
-        score1 += game1_points.get(&(b, a)).unwrap();
-        score2 += game2_points.get(&(b, a)).unwrap();
+        score1 += game1_points.get(&(a, b)).unwrap();
+        score2 += game2_points.get(&(a, b)).unwrap();
 
     }
     println!("part1: {}", score1);
