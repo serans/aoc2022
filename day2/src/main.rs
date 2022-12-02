1cc406a09b06144725d832c649406bd832c0f527
Miguel Hermo Serans <miguel.hermo@gmail.com>
2022-12-02 15:28:00 +0200

    day2 variation2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,118 +1,102 @@
-#[derive(Debug, Copy, Clone)]
-enum Shape {
-    Rock = 0,
-    Scissors = 1,
-    Paper = 2,
-}
+use std::io;
+use std::io::BufRead;
+use std::collections::HashMap;
 
-impl Shape {
 
-    fn against(&self, other: &Self) -> Result {
-        let x = *self as i32;
-        let y = *other as i32;
-        if x == y  {
-            Result::Draw
-        } else if (x+1) %3 == y {
-            Result::Win
-        } else if (y+1) %3 == x {
-            Result::Lose
-        } else {
-            panic!("wtdf {} ({}) {} ({})", x, (x+1)%3, y, (y+1)%3);
-        }
-    }
-
-    fn value(&self) -> i32 {
-        match self {
-            Shape::Rock => 1,
-            Shape::Paper => 2,
-            Shape::Scissors => 3,
-        }
-    }
-
-    fn vs(&self, other:&Self) -> i32 {
-        self.against(&other).value() + self.value()
-    }
-
-    fn get_move(&self, result: &Result) -> Shape {
-        let x = *self as i32;
-        match result {
-            Result::Lose => Shape::fromu(x+1),
-            Result::Draw => Shape::fromu(x),
-            Result::Win => Shape::fromu(x-1),
-        }
-    }
-
-    fn from(c: char) -> Shape {
+fn part1() {
+    fn shape_value(c: char) -> i32 {
         match c {
-            'A' => Shape::Rock,
-            'B' => Shape::Paper,
-            'C' => Shape::Scissors,
-            'X' => Shape::Rock,
-            'Y' => Shape::Paper,
-            'Z' => Shape::Scissors,
+            'A' => 0, 
+            'B' => 1,
+            'C' => 2,
+            'X' => 0,
+            'Y' => 1,
+            'Z' => 2,
             _ => panic!("Unknown shape")
         }
     }
 
-    fn fromu(n: i32) -> Shape {
-        let n = (n %3).abs();
-        match n {
-            0 => Shape::Rock,
-            1 => Shape::Scissors,
-            2 => Shape::Paper,
-            _ => panic!("This cannot happen {}", n)
-        } 
-    }
-
-}
-
-#[derive(Debug, Clone, Copy)]
-enum Result {
-    Lose = 0,
-    Draw = 3,
-    Win = 6,
-}
-
-impl Result {
-    fn value(&self) -> i32 {
-        *self as i32
-    }
-
-    fn from(c : char) -> Result {
-        match c {
-            'X' => Result::Lose,
-            'Y' => Result::Draw,
-            'Z' => Result::Win,
-            _ => panic!("wrong result type")
-        }
-    }
-}
-
-
-use std::io;
-use std::io::BufRead;
-
-fn main() {
     let mut score = 0;
-    let mut score2 = 0;
+    /*
+    possible games:
+     R vs R  1 + 3 = 4
+     R vs P  1 + 0 = 1
+     R vs S  1 + 6 = 7
+     P vs R  2 + 6 = 8
+     P vs P  2 + 3 = 5
+     P vs S  2 + 0 = 2
+     S vs R  3 + 0 = 3
+     S vs P  3 + 6 = 9
+     S vs S  3 + 3 = 6
+    */
+    let game_points: HashMap<(i32,i32),i32> =
+        [((0 , 0)  ,4),
+         ((0 , 1)  ,1),
+         ((0 , 2)  ,7),
+         ((1 , 0)  ,8),
+         ((1 , 1)  ,5),
+         ((1 , 2)  ,2),
+         ((2 , 0)  ,3),
+         ((2 , 1)  ,9),
+         ((2 , 2)  ,6),].iter().cloned().collect();
 
     for line in io::stdin().lock().lines().flatten() {
         if line.is_empty() {
-            continue
+            break
         }
-        let other = Shape::from(line.chars().nth(0).unwrap());
-        let me = Shape::from(line.chars().nth(2).unwrap());
-        // PROBLEM 1
-        score += me.vs(&other);
 
-        // PROBLEM 2
-        let result = Result::from(line.chars().nth(2).unwrap());
-        let my_move = other.get_move(&result);
-        println!("{}, other:{:?} vs me:{:?} => {:?}", line, other, my_move, result);
-        score2 += my_move.vs(&other);
+        // problem 1
+        let other = shape_value(line.chars().nth(0).unwrap());
+        let me = shape_value(line.chars().nth(2).unwrap());
+
+        score += game_points.get(&(me, other)).unwrap();
 
     }
 
     println!("PROBLEM 1: {}", score);
-    println!("PROBLEM 2: {}", score2);
+}
+
+fn part2() {
+    fn shape_value(c: char) -> i32 {
+        match c {
+            'A' => 0, 
+            'B' => 1,
+            'C' => 2,
+            'X' => 0,
+            'Y' => 1,
+            'Z' => 2,
+            _ => panic!("Unknown shape")
+        }
+    }
+
+    let mut score = 0;
+    let game_points: HashMap<(i32,i32),i32> =
+        [((0 , 0)  ,3+0),
+         ((0 , 1)  ,1+3),
+         ((0 , 2)  ,2+6),
+         ((1 , 0)  ,1+0),
+         ((1 , 1)  ,2+3),
+         ((1 , 2)  ,3+6),
+         ((2 , 0)  ,2+0),
+         ((2 , 1)  ,3+3),
+         ((2 , 2)  ,1+6),].iter().cloned().collect();
+
+    for line in io::stdin().lock().lines().flatten() {
+        if line.is_empty() {
+            break
+        }
+
+        // problem 1
+        let other = shape_value(line.chars().nth(0).unwrap());
+        let me = shape_value(line.chars().nth(2).unwrap());
+
+        score += game_points.get(&(me, other)).unwrap();
+
+    }
+
+    println!("PROBLEM 2: {}", score);
+}
+fn main() {
+    //part1();
+    part2();
 }
