88f73e1f5791579f367746c864a4b65262a86481
Miguel Hermo Serans <miguel.hermo@gmail.com>
2022-12-02 15:50:15 +0200

    reorder map

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -30,13 +30,13 @@ fn main() {
     let game2_points: HashMap<(u32,u32),u32> =
     // (opponent, desired_result), points_for_my_move + points_vs_opponent
         [((0, 0), 3 + 0),
-         ((0, 1), 1 + 3),
-         ((0, 2), 2 + 6),
          ((1, 0), 1 + 0),
-         ((1, 1), 2 + 3),
-         ((1, 2), 3 + 6),
          ((2, 0), 2 + 0),
+         ((0, 1), 1 + 3),
+         ((1, 1), 2 + 3),
          ((2, 1), 3 + 3),
+         ((0, 2), 2 + 6),
+         ((1, 2), 3 + 6),
          ((2, 2), 1 + 6),].iter().cloned().collect();
     
     let mut score1 = 0;
