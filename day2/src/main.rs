ac1bbaf7d153cac0c6ad78fd92c81ceccf24a0b0
Miguel Hermo Serans <miguel.hermo@gmail.com>
2022-12-02 14:47:01 +0200

    wip part2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,36 +1,27 @@
-use std::cmp::Ordering;
-
-#[derive(PartialEq, Eq)]
+#[derive(Debug, Copy, Clone)]
 enum Shape {
-    Rock,
-    Paper,
-    Scissors,
-}
-
-impl PartialOrd for Shape {
-    fn partial_cmp(&self, other:&Self) -> Option<Ordering> {
-        match &self {
-            Shape::Rock => match &other {
-                Shape::Rock     => Some(Ordering::Equal),
-                Shape::Paper    => Some(Ordering::Less),
-                Shape::Scissors => Some(Ordering::Greater),
-            },
-            Shape::Paper => match &other {
-                Shape::Rock     => Some(Ordering::Greater),
-                Shape::Paper    => Some(Ordering::Equal),
-                Shape::Scissors => Some(Ordering::Less),
-            },
-            Shape::Scissors => match &other {
-                Shape::Rock     => Some(Ordering::Less),
-                Shape::Paper    => Some(Ordering::Greater),
-                Shape::Scissors => Some(Ordering::Equal),
-            }
-        }
-    }
+    Rock = 0,
+    Scissors = 1,
+    Paper = 2,
 }
 
 impl Shape {
-    fn value(&self) -> u32 {
+
+    fn against(&self, other: &Self) -> Result {
+        let x = *self as i32;
+        let y = *other as i32;
+        if x == y  {
+            Result::Draw
+        } else if (x+1) %3 == y {
+            Result::Win
+        } else if (y+1) %3 == x {
+            Result::Lose
+        } else {
+            panic!("wtdf {} ({}) {} ({})", x, (x+1)%3, y, (y+1)%3);
+        }
+    }
+
+    fn value(&self) -> i32 {
         match self {
             Shape::Rock => 1,
             Shape::Paper => 2,
@@ -38,46 +29,90 @@ impl Shape {
         }
     }
 
-    fn score(&self, other:&Self) -> u32 {
-        if other < self {
-            6
-        } else if other > self {
-            0
-        } else {
-            3
+    fn vs(&self, other:&Self) -> i32 {
+        self.against(&other).value() + self.value()
+    }
+
+    fn get_move(&self, result: &Result) -> Shape {
+        let x = *self as i32;
+        match result {
+            Result::Lose => Shape::fromu(x+1),
+            Result::Draw => Shape::fromu(x),
+            Result::Win => Shape::fromu(x-1),
         }
     }
 
-    fn vs(&self, other:&Self) -> u32 {
-        self.score(&other) + self.value()
+    fn from(c: char) -> Shape {
+        match c {
+            'A' => Shape::Rock,
+            'B' => Shape::Paper,
+            'C' => Shape::Scissors,
+            'X' => Shape::Rock,
+            'Y' => Shape::Paper,
+            'Z' => Shape::Scissors,
+            _ => panic!("Unknown shape")
+        }
+    }
+
+    fn fromu(n: i32) -> Shape {
+        let n = (n %3).abs();
+        match n {
+            0 => Shape::Rock,
+            1 => Shape::Scissors,
+            2 => Shape::Paper,
+            _ => panic!("This cannot happen {}", n)
+        } 
+    }
+
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Result {
+    Lose = 0,
+    Draw = 3,
+    Win = 6,
+}
+
+impl Result {
+    fn value(&self) -> i32 {
+        *self as i32
+    }
+
+    fn from(c : char) -> Result {
+        match c {
+            'X' => Result::Lose,
+            'Y' => Result::Draw,
+            'Z' => Result::Win,
+            _ => panic!("wrong result type")
+        }
     }
 }
 
-fn get_shape(c : char) -> Shape {
-    match c {
-        'A' => Shape::Rock,
-        'B' => Shape::Paper,
-        'C' => Shape::Scissors,
-        'X' => Shape::Rock,
-        'Y' => Shape::Paper,
-        'Z' => Shape::Scissors,
-        _ => panic!("Unknown shape")
-    }
-}
 
 use std::io;
 use std::io::BufRead;
 
 fn main() {
-    println!("Hello, world!");
     let mut score = 0;
+    let mut score2 = 0;
+
     for line in io::stdin().lock().lines().flatten() {
         if line.is_empty() {
             continue
         }
-        let other = get_shape(line.chars().nth(0).unwrap());
-        let me = get_shape(line.chars().nth(2).unwrap());
+        let other = Shape::from(line.chars().nth(0).unwrap());
+        let me = Shape::from(line.chars().nth(2).unwrap());
+        // PROBLEM 1
         score += me.vs(&other);
+
+        // PROBLEM 2
+        let result = Result::from(line.chars().nth(2).unwrap());
+        let my_move = other.get_move(&result);
+        println!("{}, other:{:?} vs me:{:?} => {:?}", line, other, my_move, result);
+        score2 += my_move.vs(&other);
+
     }
-    println!("{}", score);
+
+    println!("PROBLEM 1: {}", score);
+    println!("PROBLEM 2: {}", score2);
 }
