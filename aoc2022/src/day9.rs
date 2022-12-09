52cf14b7b61ec985248f3ab4952561ba89265038
Miguel Hermo Serans <miguel.hermo@gmail.com>
2022-12-09 15:46:33 +0200

    refactor to use struct rather than tuple

--- aoc2022/src/day9.rs
+++ aoc2022/src/day9.rs
@@ -1,21 +1,24 @@
 use std::collections::HashSet;
 
-fn touches(p1: &(i32, i32), p2:&(i32, i32)) -> bool {
-    (p1.0-p2.0).abs()<=1 && (p1.1-p2.1).abs() <=1
-}
+#[derive(Clone, Copy, Eq, PartialEq, Hash)]
+struct Point {x:i32, y:i32}
 
-fn move_towards(origin: &(i32, i32), dest: &(i32, i32)) -> (i32, i32) {
-    let mut origin = origin.clone();
-    if origin.0 > dest.0 {origin = (origin.0-1, origin.1)}
-    if origin.0 < dest.0 {origin = (origin.0+1, origin.1)}
-    if origin.1 > dest.1 {origin = (origin.0,   origin.1-1)}
-    if origin.1 < dest.1 {origin = (origin.0,   origin.1+1)}
-    origin
+impl Point {
+    fn touches(&self, other:&Point) -> bool {
+        (self.x-other.x).abs()<=1 && (self.y-other.y).abs() <=1
+    }
+
+    fn move_towards(&mut self, dest: &Point) {
+        if self.x > dest.x { self.x-=1; }
+        if self.x < dest.x { self.x+=1; }
+        if self.y > dest.y { self.y-=1; }
+        if self.y < dest.y { self.y+=1; }
+    }
 }
 
 pub fn solve_for_n(n: usize, lines: Vec<String>) -> usize {
-    let mut rope:Vec<(i32, i32)> = vec![(0,0); n];
-    let mut tail_positions:HashSet<(i32, i32)> = HashSet::new();
+    let mut rope:Vec<Point> = vec![Point{x:0, y:0}; n];
+    let mut tail_positions:HashSet<Point> = HashSet::new();
     tail_positions.insert(*rope.last().unwrap());
 
     for command in lines {
@@ -24,22 +27,23 @@ pub fn solve_for_n(n: usize, lines: Vec<String>) -> usize {
 
         let mut head = *rope.first().unwrap();
         let head_dest = match command[0] {
-            "L" => ( head.0-distance, head.1 ),
-            "R" => ( head.0+distance, head.1 ),
-            "U" => ( head.0         , head.1+distance),
-            "D" => ( head.0         , head.1-distance),
+            "L" => Point{ x:head.x-distance, y:head.y         },
+            "R" => Point{ x:head.x+distance, y:head.y         },
+            "U" => Point{ x:head.x         , y:head.y+distance},
+            "D" => Point{ x:head.x         , y:head.y-distance},
             _ => panic!("unknown move {}", command[0])
         };
 
         while head != head_dest {
-            head = move_towards(&head, &head_dest);
+            head.move_towards(&head_dest);
             rope[0] = head;
 
             for i in 0..rope.len()-1 {
                 let front = rope[i];
-                let back = rope[i+1];
-                if !touches(&front, &back) {
-                    rope[i+1] = move_towards(&back, &front);
+                let mut back = rope[i+1];
+                if !back.touches(&front) {
+                    back.move_towards(&front);
+                    rope[i+1] = back;
                 }
             }
             tail_positions.insert(*rope.last().unwrap());
