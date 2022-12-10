e7a14fec51932099fc3f618940d4ec25599a9a8b
Miguel Hermo Serans <miguel.hermo@gmail.com>
2022-12-10 17:41:23 +0200

    refactor

--- aoc2022/src/day10.rs
+++ aoc2022/src/day10.rs
@@ -1,52 +1,64 @@
-enum Pipeline {
-    None,
-    Sum{sum:i32, del:u32},
+enum SumPipeline {
+    Inactive,
+    Running{sum:i32, delay:u32},
 }
 
 #[allow(dead_code)]
 pub fn solve(lines: impl Iterator<Item = String>) {
-    const WIDTH: i32 = 40;
-    const HEIGHT: i32 = 6;
+    // VM stuff
+    let mut x_reg = 1;
+    let mut pipeline = SumPipeline::Inactive;
+    let mut eof = false;
+    let mut cycle = 0;
 
-    let mut regx = 1;
-    let mut power = 0; // problem1 output
     let mut instructions = lines.into_iter();
+    let mut power = 0; // problem1 output
+    const WIDTH: i32 = 40; // problem2 screen width
 
-    let mut pipeline = Pipeline::None;
-    for cycle in 0..(WIDTH * HEIGHT) {
-        // process pipeline
-        if let Pipeline::Sum{sum, del} = pipeline {
-            if del == 0 {
-                regx += sum;
-                pipeline = Pipeline::None;
+    while !eof {
+        // execute
+        if let SumPipeline::Running{sum, delay} = pipeline {
+            pipeline = if delay == 0 {
+                x_reg += sum;
+                SumPipeline::Inactive
             } else {
-                pipeline = Pipeline::Sum{sum, del: del-1};
+                SumPipeline::Running{sum, delay: delay-1}
             }
         }
 
         // fetch
-        if let Pipeline::None = pipeline {
-            let new_op = instructions.next().unwrap();
-            if new_op != "noop" {
-                pipeline = Pipeline::Sum{
-                    sum: new_op.split(" ").nth(1).unwrap().parse::<i32>().unwrap(),
-                    del:1
-                };
+        if let SumPipeline::Inactive = pipeline {
+            if let Some(operation) = instructions.next() {
+                let mut op = operation.split(" ");
+                match (op.next(), op.next()) {
+                    (Some("noop"), None) => {},
+                    (Some("addx"), Some(op1)) => {
+                        pipeline = SumPipeline::Running{
+                            sum: op1.parse::<i32>().unwrap(),
+                            delay: 1
+                        }
+                    }
+                    _ => {panic!("unknown command {}", operation)}
+                }
+            } else {
+                eof = true;
             }
         }
 
         // problem1
         if [20, 60, 100, 140, 180, 220].contains(&(cycle+1)){
-            power += regx*(cycle+1);
+            power += x_reg*(cycle+1);
         }
 
         // problem2
         let x = cycle % WIDTH;
-        let point = if [regx-1, regx, regx+1].contains(&x) { '#' } else { ' ' };
+        let point = if [x_reg-1, x_reg, x_reg+1].contains(&x) { '#' } else { ' ' };
         print!("{point}");
         if x == WIDTH-1 {
             println!("")
         }
+
+        cycle += 1;
     }
     println!("problem1 {}", power);
 }
