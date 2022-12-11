226598e53632207c4f76630cef7c3c496f52d529
Miguel Hermo Serans <miguel.hermo@gmail.com>
2022-12-11 15:54:38 +0200

    day 11

--- aoc2022/src/main.rs
+++ aoc2022/src/main.rs
@@ -8,6 +8,7 @@ mod day7;
 mod day8;
 mod day9;
 mod day10;
+mod day11;
 
 use std::fs::File;
 use std::io::{self, BufRead};
@@ -32,5 +33,6 @@ fn main() {
     //day7::solve(read_lines("input/day7.txt").flatten());
     //day8::solve(read_lines("input/day8.txt").flatten());
     //day9::solve(read_lines("input/day9.txt").flatten());
-    day10::solve(read_lines("input/day10.txt").flatten());
+//    day10::solve(read_lines("input/day10.txt").flatten());
+    day11::solve();
 }
